1f0bd160dbec4f5d44589f299bede6b40cd86b3b
Matziaz PI <matiaspiedrapichardo@gmail.com>
2023-01-08 21:48:42 -0600

    first commit

---
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,31 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
+import frc.robot.Constants.DrivetrainConstants;
+
+public class Drivetrain extends SubsystemBase {
+  
+private final CANSparkMax m_frontLeft = new CANSparkMax(DrivetrainConstants.kMotorPorts[0],
+    CANSparkMaxLowLevel.MotorType.kBrushless);
+    
+private final CANSparkMax m_rearLeft = new CANSparkMax(DrivetrainConstants.kMotorPorts[1],
+    CANSparkMaxLowLevel.MotorType.kBrushless);
+
+private final CANSparkMax m_frontRight = new CANSparkMax(DrivetrainConstants.kMotorPorts[2],
+    CANSparkMaxLowLevel.MotorType.kBrushless);
+    
+private final CANSparkMax m_rearRight = new CANSparkMax(DrivetrainConstants.kMotorPorts[3],
+    CANSparkMaxLowLevel.MotorType.kBrushless);
+
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
