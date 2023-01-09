9572041ef3259ba3f7ea60c723f9e81bb4531e12
LH9301 <hernandezreynaluis@gmail.com>
2023-01-09 10:27:46 -0600

    Drivetrain phase 1

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -4,24 +4,39 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.kauailabs.navx.frc.AHRS;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel;
+
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.DrivetrainConstants;
+import frc.robot.commands.MoveDrivetrain;
 
 public class Drivetrain extends SubsystemBase {
   
-private final CANSparkMax m_frontLeft = new CANSparkMax(DrivetrainConstants.kMotorPorts[0],
+  private final CANSparkMax m_frontLeft = new CANSparkMax(DrivetrainConstants.kMotorPorts[0],
     CANSparkMaxLowLevel.MotorType.kBrushless);
     
-private final CANSparkMax m_rearLeft = new CANSparkMax(DrivetrainConstants.kMotorPorts[1],
+  private final CANSparkMax m_rearLeft = new CANSparkMax(DrivetrainConstants.kMotorPorts[1],
     CANSparkMaxLowLevel.MotorType.kBrushless);
 
-private final CANSparkMax m_frontRight = new CANSparkMax(DrivetrainConstants.kMotorPorts[2],
+  private final CANSparkMax m_frontRight = new CANSparkMax(DrivetrainConstants.kMotorPorts[2],
     CANSparkMaxLowLevel.MotorType.kBrushless);
     
-private final CANSparkMax m_rearRight = new CANSparkMax(DrivetrainConstants.kMotorPorts[3],
+  private final CANSparkMax m_rearRight = new CANSparkMax(DrivetrainConstants.kMotorPorts[3],
     CANSparkMaxLowLevel.MotorType.kBrushless);
+
+  private final AHRS gyro = new AHRS (SPI.Port.kMXP);
+
+  private final MecanumDrive m_drive =
+    new MecanumDrive(m_frontLeft, m_rearLeft, m_frontRight, m_rearRight);
+
+  public Drivetrain () {
+    this.setDefaultCommand(new MoveDrivetrain(this));
+  }
 
   @Override
   public void periodic() {
