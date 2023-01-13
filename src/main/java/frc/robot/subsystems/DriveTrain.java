3bf0205e337427f18f84abeae07e0fd5014cc5e4
LH9301 <hernandezreynaluis@gmail.com>
2023-01-13 12:24:10 -0600

    Differential Drive

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -5,34 +5,11 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel;
-import frc.robot.Constants.DrivetrainConstants;
-import com.revrobotics.RelativeEncoder;
-import edu.wpi.first.wpilibj.Timer;
-
 
 public class Drivetrain extends SubsystemBase {
-  
-private final CANSparkMax m_frontLeft = new CANSparkMax(DrivetrainConstants.kMotorPorts[0],
-    CANSparkMaxLowLevel.MotorType.kBrushless);
-    
-private final CANSparkMax m_rearLeft = new CANSparkMax(DrivetrainConstants.kMotorPorts[1],
-    CANSparkMaxLowLevel.MotorType.kBrushless);
+  /** Creates a new Drivetrain. */
+  public Drivetrain() {}
 
-private final CANSparkMax m_frontRight = new CANSparkMax(DrivetrainConstants.kMotorPorts[2],
-    CANSparkMaxLowLevel.MotorType.kBrushless);
-    
-private final CANSparkMax m_rearRight = new CANSparkMax(DrivetrainConstants.kMotorPorts[3],
-    CANSparkMaxLowLevel.MotorType.kBrushless);
-
-private RelativeEncoder m_leftEncoder;
-private RelativeEncoder m_rightEncoder;
-
-private final Timer timer = new Timer();
-
-
-    
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
