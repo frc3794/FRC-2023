1eb663231f85a51e98f310120a5a58c17dc914b0
TaniaNya <117779279+TaniaNya@users.noreply.github.com>
2023-02-22 16:59:15 -0600

    claw

--- src/main/java/frc/robot/subsystems/Claw.java
+++ src/main/java/frc/robot/subsystems/Claw.java
@@ -5,6 +5,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.CANSparkMax; 
 import frc.robot.Constants.DrivetrainConstants;
 import com.revrobotics.CANSparkMaxLowLevel;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 public class Claw extends SubsystemBase{
 
@@ -32,6 +33,32 @@ public static Command StopMotors(){
     m_clawMotor.stopMotor();
     return null;
 }
+
+public static boolean isPressed;
+
+private static final VictorSPX O_CLAW_VICTOR_SPX = new VictorSPX(0)
+
+public static Command ExtendMovileStage(){
+    m_movileStageMotor.set(ControlMode.PercentOutput, .75);
+    return null;
+  }
+  public static Command RetractMovileStage(){
+    m_movileStageMotor.set(ControlMode.PercentOutput, -.75);
+    return null;
+  }
+  public static void GetLimits(){
+
+    if (m_forwardLimit.get() || m_reverseLimit.get()){
+      isPressed = true;
+
+    } else {
+      isPressed = false;
+    }
+    
+    if (isPressed){
+      m_movileStageMotor.set(ControlMode.PercentOutput, 0);
+    } 
+  }
 }
 
 
