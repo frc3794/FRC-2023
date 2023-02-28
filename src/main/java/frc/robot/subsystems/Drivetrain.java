750107d35b201fe36aa7c456d468ee85c4160f1e
LH9301 <hernandezreynaluis@gmail.com>
2023-02-28 10:14:20 -0600

    Fix Zya

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -4,14 +4,46 @@
 
 package frc.robot.subsystems;
 
+import com.kauailabs.navx.frc.AHRS;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
+
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.DrivetrainConstants;
+import com.revrobotics.RelativeEncoder;
+import edu.wpi.first.wpilibj.Timer;
+import frc.robot.commands.MoveDrivetrain;
 
 public class Drivetrain extends SubsystemBase {
-  /** Creates a new Drivetrain. */
-  public Drivetrain() {}
+  
+  private final CANSparkMax m_frontLeft = new CANSparkMax(DrivetrainConstants.kMotorPorts[0],
+    CANSparkMaxLowLevel.MotorType.kBrushless);
+    
+  private final CANSparkMax m_rearLeft = new CANSparkMax(DrivetrainConstants.kMotorPorts[1],
+    CANSparkMaxLowLevel.MotorType.kBrushless);
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
+  private final CANSparkMax m_frontRight = new CANSparkMax(DrivetrainConstants.kMotorPorts[2],
+    CANSparkMaxLowLevel.MotorType.kBrushless);
+    
+  private final CANSparkMax m_rearRight = new CANSparkMax(DrivetrainConstants.kMotorPorts[3],
+    CANSparkMaxLowLevel.MotorType.kBrushless);
+
+  private RelativeEncoder m_rleftEncoder;
+  private RelativeEncoder m_rrightEncoder;
+  private RelativeEncoder m_fleftEncoder;
+  private RelativeEncoder m_frightEncoder;
+
+  private final Timer timer = new Timer();
+
+  private final AHRS gyro = new AHRS (SPI.Port.kMXP);
+
+  private final MecanumDrive m_drive =
+  new MecanumDrive(m_frontLeft, m_rearLeft, m_frontRight, m_rearRight);
+
+  public Drivetrain () {
+  this.setDefaultCommand(new MoveDrivetrain());
   }
+
 }
