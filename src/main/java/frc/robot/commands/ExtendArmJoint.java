d4b4cfc025e2c2c1539a38bd0490502d2952b683
LH9301 <hernandezreynaluis@gmail.com>
2023-03-11 20:57:36 -0600

    Finished Phase 2

--- src/main/java/frc/robot/commands/ExtendArmJoint.java
+++ src/main/java/frc/robot/commands/ExtendArmJoint.java
@@ -3,8 +3,11 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.commands;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Robot;
 import frc.robot.subsystems.ArmJoint;
 
@@ -12,6 +15,11 @@ public class ExtendArmJoint extends CommandBase {
 
   private final CommandXboxController m_subSystemController = Robot.getSubsystemController();
   private final ArmJoint m_armJoint;
+
+  private final double speed = 0.5;
+
+  private final Trigger m_btnA = m_subSystemController.a();
+  private final Trigger m_btnY = m_subSystemController.y();
   
   public ExtendArmJoint(ArmJoint armJoint) {
     this.m_armJoint = armJoint;
@@ -23,14 +31,27 @@ public class ExtendArmJoint extends CommandBase {
 
   @Override
   public void execute() {
-    double m_armJointMovement = Math.abs (m_subSystemController.getRightTriggerAxis());
-    double m_armJointMovement2 = Math.abs (m_subSystemController.getLeftTriggerAxis());
-    
+    double m_armJointMovement = m_subSystemController.getRightY() * this.speed;
+
     if (m_armJointMovement > 0.2){
       m_armJoint.extendArmJoint(m_armJointMovement);
-    } else if (m_armJointMovement2 < -0.2){
+    } else if (m_armJointMovement < -0.2){
       m_armJoint.flexArmJoint(m_armJointMovement);
+    } else {
+      m_btnA.onTrue(extendArm());
+      m_btnY.onTrue(flexArm());
     }
+
+  }
+
+  private Command extendArm () {
+    m_armJoint.extendArmJoint(this.speed);
+    return new WaitCommand (0.00);
+  }
+
+  private Command flexArm () {
+    m_armJoint.flexArmJoint(this.speed);
+    return new WaitCommand (0.00);
   }
 
   @Override
